6e55b1c5ad50172dab69c38156fb6a66e6be7cae
shawnenterprises <hangmananimations999@gmail.com>
2026-03-19 14:18:37 -0700

    Add mouseover effect to links and buttons

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll("a, button").forEach(el => {
+  el.addEventListener("mouseover", () => {
+    el.style.letterSpacing = "2px";
+    setTimeout(() => {
+      el.style.letterSpacing = "0px";
+    }, 100);
+  });
+});
