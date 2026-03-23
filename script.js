f3b21808249677f957def43f9cd2e0ebff0d7267
shawnenterprises <hangmananimations999@gmail.com>
2026-03-22 21:06:21 -0700

    Add citizen status display and registration function

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ window.onload = () => {
     document.getElementById("intro").style.display = "none";
   }, 1200);
 };
+
+let user = localStorage.getItem("citizen");
+
+if(user){
+  document.body.insertAdjacentHTML("afterbegin",
+  "<div style='position:fixed; top:0; right:0; font-size:10px; padding:5px;'>STATUS: CITIZEN</div>");
+}
+
+function register() {
+  localStorage.setItem("citizen", "true");
+
+  document.getElementById("status").innerText =
+  "STATUS: REGISTERED CITIZEN";
+}
