b9c625456e711e00d61bd973a248178b0084e1cd
shawnenterprises <hangmananimations999@gmail.com>
2026-03-22 20:46:11 -0700

    Add cart management functions with localStorage
    Implement cart functionality with localStorage support

--- cart.js
+++ cart.js
@@ -1,1 +1,16 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+function addToCart(product) {
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+  alert("Added to cart");
+}
+
+function updateCartCount() {
+  document.querySelectorAll("#cart-count").forEach(el => {
+    el.innerText = cart.length;
+  });
+}
+
+updateCartCount();
